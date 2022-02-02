bd93f06cca79c4d55a7553104d300a3381d1de7e
Hejing Li <hajeongee@gmail.com>
2022-02-02 16:39:39 +0100

    intetrated config script memory size fix

--- experiments/pyexps/gem5E1k.sh
+++ experiments/pyexps/gem5E1k.sh
@@ -4,15 +4,15 @@ REPO_DIR="/OS/endhost-networking/work/sim/hejing/simbricks"
 GEM5_EXE="$REPO_DIR/sims/external/gem5/build/X86/gem5.opt"
 SW_EXE="$REPO_DIR/sims/net/switch/net_switch"
 
-OUT_DIR_SER="$REPO_DIR/experiments/out/gem5Nic/0/gem5-out.server.0"
-OUT_DIR_CL="$REPO_DIR/experiments/out/gem5Nic/0/gem5-out.client.0"
-OUT_DIR_BASE="$REPO_DIR/experiments/out/gem5Nic/0"
+OUT_DIR_SER="$REPO_DIR/experiments/out/gem5Nic/3/gem5-out.server.0"
+OUT_DIR_CL="$REPO_DIR/experiments/out/gem5Nic/3/gem5-out.client.0"
+OUT_DIR_BASE="$REPO_DIR/experiments/out/gem5Nic/3"
 
 CKP_DIR_SER="$REPO_DIR/experiments/out/gem5Nic/0/gem5-cp.server.0"
 CKP_DIR_CL="$REPO_DIR/experiments/out/gem5Nic/0/gem5-cp.client.0"
 
 GEM5_CONFIG="$REPO_DIR/sims/external/gem5/configs/simbricks/simbricks.py"
-X86_ARGS=" --caches --l2cache --l3cache --l1d_size=32kB --l1i_size=32kB --l2_size=2MB --l3_size=32MB --l1d_assoc=8 --l1i_assoc=8 --l2_assoc=4 --l3_assoc=16 --cacheline_size=64 --cpu-clock=3GHz --sys-clock=1GHz --cpu-type=TimingSimpleCPU --mem-size=892MB --num-cpus=1 --ddio-enabled --ddio-way-part=8 --mem-type=DDR4_2400_16x4"
+X86_ARGS=" --caches --l2cache --l3cache --l1d_size=32kB --l1i_size=32kB --l2_size=2MB --l3_size=32MB --l1d_assoc=8 --l1i_assoc=8 --l2_assoc=4 --l3_assoc=16 --cacheline_size=64 --cpu-clock=3GHz --sys-clock=1GHz --cpu-type=TimingSimpleCPU --mem-size=8192MB --num-cpus=1 --ddio-enabled --ddio-way-part=8 --mem-type=DDR4_2400_16x4"
 
 KERNEL_DIR="$REPO_DIR/images/vmlinux"
 IMAGE="$REPO_DIR/images/output-base/base.raw"
@@ -27,7 +27,7 @@ SHM_SER="$REPO_DIR/experiments/out/gem5Nic/0/dev.shm.server.0."
 ETH_CL="$REPO_DIR/experiments/out/gem5Nic/0/dev.eth.clinet.0."
 SHM_CL="$REPO_DIR/experiments/out/gem5Nic/0/dev.shm.client.0."
 
-DEBUG_FLAGS="Ethernet,DMA,PciDevice"
+DEBUG_FLAGS="Ethernet,DMA,PciDevice,EthernetIntr"
 
 ALL_PIDS=""
 WAIT_PIDS=""
@@ -37,7 +37,7 @@ run_gem5()
     echo "start server"
     $GEM5_EXE --outdir=$OUT_DIR_SER \
                 --debug-flags=$DEBUG_FLAGS \
-                --debug-start=728314941261 \
+                --debug-start=1276516223792 \
                 $GEM5_CONFIG \
                 $X86_ARGS \
                 --checkpoint-dir=$CKP_DIR_SER \
@@ -54,7 +54,7 @@ run_gem5()
     echo "start client"
     $GEM5_EXE --outdir=$OUT_DIR_CL \
                 --debug-flags=$DEBUG_FLAGS \
-                --debug-start=728314941261 \
+                --debug-start=1276516223792 \
                 $GEM5_CONFIG \
                 $X86_ARGS \
                 --checkpoint-dir=$CKP_DIR_CL \
@@ -99,6 +99,8 @@ sighandler(){
 
 trap "sighandler" SIGINT
 
+mkdir -p $OUT_DIR_SER
+mkdir -p $OUT_DIR_CL
 rm  $OUT_DIR_BASE/*shm* $OUT_DIR_BASE/*eth*
 rm $OUT_DIR_BASE/run.out
 echo "start time" >> $OUT_DIR_BASE/run.out
