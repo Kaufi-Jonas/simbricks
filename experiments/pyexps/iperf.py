3c42dd83e223e94a1a0e21ec92c7cb26fb195ca4
Jonas Kaufmann <jokau03@gmail.com>
2023-08-20 21:47:53 +0200

    iperf.py: add more configurations

--- experiments/pyexps/iperf.py
+++ experiments/pyexps/iperf.py
@@ -20,18 +20,19 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-Two hosts connected by a switch. Both run iPerf where one host is the client and
-the other the server.
+This module defines multiple experiments for different virtual test bed
+configurations containing two hosts running iPerf to determine throughput and
+latency.
 
-This module creates multiple experiments for comparing multiple host and NIC
-simulator combinations. Client and Server use the same simulators.
+One of these hosts acts as the iPerf server and the other as the client.
 """
 
 from simbricks.orchestration import experiments as exp
-from simbricks.orchestration import nodeconfig, simulator_utils, simulators
+from simbricks.orchestration import nodeconfig, simulators
 
-host_types = ['qemu', 'gem5', 'simics']
-nic_types = ['i40e', 'e1000']
+host_types = ['qemu', 'qt', 'gem5', 'simics']
+nic_types = ['i40e', 'e1000', 'cd_bm', 'cd_verilator']
+net_types = ['wire', 'switch', 'ns3_bridge', 'ns3_dumbell']
 experiments = []
 
 
@@ -42,64 +43,89 @@ class QemuTiming(simulators.QemuHost):
         self.sync = True
 
 
-class Gem5Timing(simulators.Gem5Host):
+for host_t in host_types:
+    for nic_t in nic_types:
+        for net_t in net_types:
+            e = exp.Experiment(f'iperf-{host_t}-{nic_t}-{net_t}')
 
-    def __init__(self, node_config):
-        super().__init__(node_config)
-        self.cpu_type = 'TimingSimpleCPU'
-        self.cpu_type_cp = 'TimingSimpleCPU'
+            # host simulator
+            sync = True
+            if host_t == 'qemu':
+                HostClass = simulators.QemuHost
+                sync = False
+            elif host_t == 'qt':
+                HostClass = QemuTiming
+            elif host_t == 'gem5':
+                HostClass = simulators.Gem5Host
+                e.checkpoint = True
+            elif host_t == 'simics':
+                HostClass = simulators.SimicsHost
+                e.checkpoint = True
+            else:
+                raise NameError(f'No host simulator for key {host_t}.')
 
+            # NIC simulator
+            if nic_t == 'i40e':
+                NicClass = simulators.I40eNIC
+                NodeConfigClass = nodeconfig.I40eLinuxNode
+            elif nic_t == 'e1000':
+                NicClass = simulators.E1000NIC
+                NodeConfigClass = nodeconfig.E1000LinuxNode
+            elif nic_t == 'cd_bm':
+                NicClass = simulators.CorundumBMNIC
+                NodeConfigClass = nodeconfig.CorundumLinuxNode
+            elif nic_t == 'cd_verilator':
+                NicClass = simulators.CorundumVerilatorNIC
+                NodeConfigClass = nodeconfig.CorundumLinuxNode
+            else:
+                raise NameError(f'No NIC simulator for key {nic_t}.')
 
-for host_type in host_types:
-    for nic_type in nic_types:
-        e = exp.Experiment(f'iperf-{host_type}-{nic_type}-pair')
-        net = simulators.SwitchNet()
-        e.add_network(net)
+            # network simulator
+            if net_t == 'wire':
+                net = simulators.WireNet()
+            elif net_t == 'switch':
+                net = simulators.SwitchNet()
+            elif net_t == 'ns3_bridge':
+                net = simulators.NS3BridgeNet()
+            elif net_t == 'ns3_dumbell':
+                net = simulators.NS3DumbbellNet()
+            else:
+                raise NameError(f'No net simulator for key {net_t}.')
+            if sync:
+                net.sync_mode = 1
+            e.add_network(net)
 
-        if host_type == 'qemu':
-            HostClass = QemuTiming
-        elif host_type == 'gem5':
-            HostClass = Gem5Timing
-            e.checkpoint = True
-        elif host_type == 'simics':
-            HostClass = simulators.SimicsHost
-        else:
-            raise NameError(host_type)
+            # set up server
+            server_nic = NicClass()
+            server_nic.set_network(net)
+            if sync:
+                server_nic.sync_mode = 1
+            server_nc = NodeConfigClass()
+            server_nc.ip = '10.0.0.1'
+            server_nc.app = nodeconfig.IperfTCPServer()
 
-        if nic_type == 'i40e':
-            NicClass = simulators.I40eNIC
-            NodeConfigClass = nodeconfig.I40eLinuxNode
-        elif nic_type == 'e1000':
-            NicClass = simulators.E1000NIC
-            NodeConfigClass = nodeconfig.E1000LinuxNode
-        else:
-            raise NameError(nic_type)
+            server = HostClass(server_nc)
+            server.name = 'server.0'
+            server.add_nic(server_nic)
+            e.add_nic(server_nic)
+            e.add_host(server)
 
-        servers = simulator_utils.create_basic_hosts(
-            e,
-            1,
-            'server',
-            net,
-            NicClass,
-            HostClass,
-            NodeConfigClass,
-            nodeconfig.IperfTCPServer
-        )
+            # set up client
+            client_nic = NicClass()
+            client_nic.set_network(net)
+            if sync:
+                client_nic.sync_mode = 1
+            client_nc = NodeConfigClass()
+            client_nc.ip = '10.0.0.2'
+            client_nc.app = nodeconfig.IperfTCPClient()
+            client_nc.app.server_ip = server_nc.ip
+            client_nc.app.is_last = True
 
-        clients = simulator_utils.create_basic_hosts(
-            e,
-            1,
-            'client',
-            net,
-            NicClass,
-            HostClass,
-            NodeConfigClass,
-            nodeconfig.IperfTCPClient,
-            ip_start=2
-        )
+            client = HostClass(client_nc)
+            client.name = 'client.0'
+            client.add_nic(client_nic)
+            e.add_nic(client_nic)
+            e.add_host(client)
 
-        for c in clients:
-            c.wait = True
-            c.node_config.app.server_ip = servers[0].node_config.ip
-
-        experiments.append(e)
+            client.wait = True
+            experiments.append(e)
