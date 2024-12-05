39aa49b9fedeb938c55f7a094708d0e8fe264310
Jonas Kaufmann <jokau03@gmail.com>
2024-12-05 09:49:34 +0100

    pyexps/classify_simple: AC/DSim changes

--- experiments/pyexps/classify_simple.py
+++ experiments/pyexps/classify_simple.py
@@ -10,12 +10,13 @@ experiments = []
 
 # Experiment parameters
 host_variants = ["qk", "qt", "gt", "gk", "ga", "simics"]
+rtl_variants = ["verilator", "gate", "rtl"]
 inference_device_opts = [
     node.TvmDeviceType.VTA,
     node.TvmDeviceType.CPU,
-    node.TvmDeviceType.CPU_ARM64
+    node.TvmDeviceType.CPU_ARM64,
 ]
-vta_clk_freq_opts = [100, 200, 400]
+vta_clk_freq_opts = [100, 167]
 vta_batch_opts = [1, 2]
 vta_block_opts = [16]
 model_name_opts = [
@@ -45,35 +46,36 @@ class TvmClassifyLocal(node.AppConfig):
     def config_files(self):
         # mount TVM inference script in simulated server under /tmp/guest
         files = {
-            "deploy_classification-infer.py":
-                open(
-                    "/home/jonask/Repos/tvm-simbricks/vta/tutorials/frontend/deploy_classification-infer.py",
-                    "rb",
-                ),
-            "cat.jpg":
-                open("/home/jonask/Downloads/cat.jpg", "rb"),
+            "deploy_classification-infer.py": open(
+                "/home/jonask/Repos/tvm-simbricks/vta/tutorials/frontend/deploy_classification-infer.py",
+                "rb",
+            ),
+            "cat.jpg": open("/home/jonask/Downloads/cat.jpg", "rb"),
         }
         for library in os.listdir(self.mxnet_dir):
             if not library.endswith(".so"):
                 continue
             files[library] = open(f"{self.mxnet_dir}/{library}", "rb")
         files["cma_malloc.ko"] = open(
-            "/home/jonask/Repos/cma_malloc/module/cma_malloc.ko", "rb"
+            "/home/jonask/Repos/tvm-simbricks/3rdparty/cma_malloc/module/cma_malloc.ko",
+            "rb",
         )
         return files
 
     def prepare_pre_cp(self) -> list[str]:
         cmds = super().prepare_pre_cp()
-        cmds.extend([
-            'echo \'{"TARGET" : "simbricks-pci", "HW_VER" : "0.0.2",'
-            ' "LOG_INP_WIDTH" : 3, "LOG_WGT_WIDTH" : 3,'
-            ' "LOG_ACC_WIDTH" : 5, "LOG_BATCH" :'
-            f' {int(math.log2(self.vta_batch))}, "LOG_BLOCK" :'
-            f' {int(math.log2(self.vta_block))}, "LOG_UOP_BUFF_SIZE" :'
-            ' 15, "LOG_INP_BUFF_SIZE" : 15, "LOG_WGT_BUFF_SIZE" : 18,'
-            ' "LOG_ACC_BUFF_SIZE" : 17 }\' >'
-            " /root/tvm/3rdparty/vta-hw/config/vta_config.json"
-        ])
+        cmds.extend(
+            [
+                'echo \'{"TARGET" : "simbricks-pci", "HW_VER" : "0.0.2",'
+                ' "LOG_INP_WIDTH" : 3, "LOG_WGT_WIDTH" : 3,'
+                ' "LOG_ACC_WIDTH" : 5, "LOG_BATCH" :'
+                f' {int(math.log2(self.vta_batch))}, "LOG_BLOCK" :'
+                f' {int(math.log2(self.vta_block))}, "LOG_UOP_BUFF_SIZE" :'
+                ' 15, "LOG_INP_BUFF_SIZE" : 15, "LOG_WGT_BUFF_SIZE" : 18,'
+                ' "LOG_ACC_BUFF_SIZE" : 17 }\' >'
+                " /root/tvm/3rdparty/vta-hw/config/vta_config.json"
+            ]
+        )
         for library in os.listdir(self.mxnet_dir):
             if not library.endswith(".so"):
                 continue
@@ -83,40 +85,46 @@ class TvmClassifyLocal(node.AppConfig):
 
     def run_cmds(self, node):
         cmds = []
-        print(self.target_host, self.target_device)
         if self.target_device.is_cpu():
-            cmds.extend([
-                "python3 -m tvm.exec.rpc_server --port=9091 &", "sleep 12"
-            ])
+            cmds.extend(
+                ["python3 -m tvm.exec.rpc_server --port=9091 &", "sleep 6"]
+            )
         else:
-            cmds.extend([
-                f"VTA_DEVICE=0000:00:{(self.pci_vta_id):02x}.0 python3 -m"
-                " vta.exec.rpc_server &"
-                # wait for RPC server to start
-                "sleep 10"
-            ])
+            cmds.extend(
+                [
+                    (
+                        f"VTA_DEVICE=0000:00:{(self.pci_vta_id):02x}.0"
+                        " VTA_DRY_RUN_FILE=/tmp/vta_dry_run python3 -m"
+                        " vta.exec.rpc_server --port=9091 &"
+                    ),
+                    "sleep 12",
+                ]
+            )
         # define commands to run on simulated server
-        cmds.extend([
-            "export VTA_RPC_HOST=127.0.0.1",
-            "export VTA_RPC_PORT=9091",
-            "export SIMULATOR=gem5",  # TODO Actually set this depending on simulator
-            "m5 checkpoint",
-            # run warmup inference
-            (
-                "python3 /tmp/guest/deploy_classification-infer.py /root/mxnet"
-                f" {self.target_device.value} {self.target_host.value} {self.model_name} /tmp/guest/cat.jpg"
-                f" {self.batch_size} {self.repetitions} {int(self.debug)} 0"
-            ),
-            # dump stats every 100 ms
-            "m5 resetstats",
-            "m5 dumpstats 100000000 100000000",
-            # run actual inference
-            (
-                "python3 /tmp/guest/deploy_classification-infer.py /root/mxnet"
-                f" {self.target_device.value} {self.target_host.value} {self.model_name} /tmp/guest/cat.jpg"
-                f" {self.batch_size} {self.repetitions} {int(self.debug)} 0"
-            ),
-        ])
+        cmds.extend(
+            [
+                "export VTA_RPC_HOST=127.0.0.1",
+                "export VTA_RPC_PORT=9091",
+                "m5 checkpoint",
+                # run warmup inference
+                "touch /tmp/vta_dry_run",
+                (
+                    "python3 /tmp/guest/deploy_classification-infer.py"
+                    " /root/mxnet"
+                    f" {self.target_device.value} {self.target_host.value} {self.model_name} /tmp/guest/cat.jpg"
+                    f" {self.batch_size} {self.repetitions} {int(self.debug)} 0"
+                ),
+                "rm /tmp/vta_dry_run",
+                "export SIMULATOR=gem5",  # TODO Actually set this depending on simulator
+                # run actual inference
+                (
+                    "python3 /tmp/guest/deploy_classification-infer.py"
+                    " /root/mxnet"
+                    f" {self.target_device.value} {self.target_host.value} {self.model_name} /tmp/guest/cat.jpg"
+                    f" {self.batch_size} {self.repetitions} {int(self.debug)} 0"
+                ),
+            ]
+        )
         return cmds
 
 
@@ -125,7 +133,7 @@ class VtaNode(node.NodeConfig):
     def __init__(self) -> None:
         super().__init__()
         # Use locally built disk image
-        self.disk_image = "vta_classification"
+        self.disk_image = "vta"
         # Bump amount of system memory
         self.memory = 2 * 1024
         # Reserve physical range of memory for the VTA user-space driver
@@ -134,25 +142,27 @@ class VtaNode(node.NodeConfig):
     def prepare_pre_cp(self):
         # Define commands to run before application to configure the server
         cmds = super().prepare_pre_cp()
-        cmds.extend([
-            "mount -t proc proc /proc",
-            "mount -t sysfs sysfs /sys",
-            # Make TVM's Python framework available
-            "export PYTHONPATH=/root/tvm/python:${PYTHONPATH}",
-            "export PYTHONPATH=/root/tvm/vta/python:${PYTHONPATH}",
-            "export MXNET_HOME=/root/mxnet",
-            # Set up loopback interface so the TVM inference script can
-            # connect to the RPC server
-            "ip link set lo up",
-            "ip addr add 127.0.0.1/8 dev lo",
-            # Make VTA device available for control from user-space via
-            # VFIO
-            (
-                "echo 1"
-                " >/sys/module/vfio/parameters/enable_unsafe_noiommu_mode"
-            ),
-            'echo "dead beef" >/sys/bus/pci/drivers/vfio-pci/new_id',
-        ])
+        cmds.extend(
+            [
+                "mount -t proc proc /proc",
+                "mount -t sysfs sysfs /sys",
+                # Make TVM's Python framework available
+                "export PYTHONPATH=/root/tvm/python:${PYTHONPATH}",
+                "export PYTHONPATH=/root/tvm/vta/python:${PYTHONPATH}",
+                "export MXNET_HOME=/root/mxnet",
+                # Set up loopback interface so the TVM inference script can
+                # connect to the RPC server
+                "ip link set lo up",
+                "ip addr add 127.0.0.1/8 dev lo",
+                # Make VTA device available for control from user-space via
+                # VFIO
+                (
+                    "echo 1"
+                    " >/sys/module/vfio/parameters/enable_unsafe_noiommu_mode"
+                ),
+                'echo "dead beef" >/sys/bus/pci/drivers/vfio-pci/new_id',
+            ]
+        )
         return cmds
 
 
@@ -164,7 +174,8 @@ for (
     vta_batch,
     vta_block,
     model_name,
-    cores
+    cores,
+    rtl_variant,
 ) in itertools.product(
     host_variants,
     inference_device_opts,
@@ -172,10 +183,11 @@ for (
     vta_batch_opts,
     vta_block_opts,
     model_name_opts,
-    core_opts
+    core_opts,
+    rtl_variants,
 ):
     experiment = exp.Experiment(
-        f"cs-{model_name}-{inference_device.value}-{host_var}-{cores}-{vta_clk_freq}-{vta_batch}x{vta_block}"
+        f"cs-{model_name}-{inference_device.value}-{host_var}-{cores}-{vta_clk_freq}-{vta_batch}x{vta_block}-{rtl_variant}"
     )
     pci_vta_id = 2
     sync = False
@@ -199,18 +211,17 @@ for (
                 self.cpu_freq = "1200MHz"
                 self.cpu_type = "hpi"
                 self.variant = "fast"
-                # self.extra_script_args.append("--write-terminal-output")
 
         HostClass = CustomGem5ArmHost
         experiment.checkpoint = True
-        sync = False
+        sync = True
     elif host_var == "gk":
         pci_vta_id = 0
         HostClass = sim.Gem5KvmHost
-        sync = True
+        sync = False
     elif host_var == "simics":
         HostClass = sim.SimicsHost
-        pci_vta_id = 0x0b
+        pci_vta_id = 0x0B
 
     # Instantiate server
     server_cfg = VtaNode()
@@ -238,10 +249,33 @@ for (
 
     # Instantiate and connect VTA PCIe-based accelerator to server
     if inference_device == node.TvmDeviceType.VTA:
-        vta = sim.VTADev()
+        if rtl_variant == "verilator":
+            vta = sim.VTADev()
+            vta.batch = vta_batch
+            vta.block = vta_block
+        elif rtl_variant == "gate":
+            vta = sim.XsimDev(
+                "vta_xsim",
+                100,
+                "/home/jonask/Repos/vivado_vta/vivado_vta.sim/sim_1/synth/func/xsim/vta_sim_vlog.prj",
+                "vta_sim",
+            )
+            vta.saif_sampling_period_ns = 10 * 10**6
+            vta.saif_sampling_length_ns = 10 * 10**6
+        elif rtl_variant == "rtl":
+            vta = sim.XsimDev(
+                "vta_xsim_rtl",
+                100,
+                "/home/jonask/Repos/vivado_vta/vivado_vta.sim/sim_1/behav/xsim/vta_sim_behav_vlog.prj",
+                "vta_sim_behav",
+            )
+            vta.libs = []
+            vta.saif_sampling_period_ns = 10 * 10**6
+            vta.saif_sampling_length_ns = 10 * 10**6
+        else:
+            raise NameError(f"Unknown rtl_variant {rtl_variant}")
         vta.clock_freq = vta_clk_freq
-        vta.batch = vta_batch
-        vta.block = vta_block
+
         server.add_pcidev(vta)
         if host_var == "simics":
             server.debug_messages = False
@@ -249,7 +283,7 @@ for (
 
     server.pci_latency = server.sync_period = vta.pci_latency = (
         vta.sync_period
-    ) = 500
+    ) = 200
 
     # Add both simulators to experiment
     experiment.add_host(server)
